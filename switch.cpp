9ec04668e62d184ef804607d9a419f8f8e9730dc
Sageena Sharma <sharmasageena@gmail.com>
2023-12-29 11:33:33 +0530

    changes

---
+++ switch.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=6;
+    cout<<endl;
+    switch (num)
+    {
+    case 1: cout<<"no. is one"<<endl;
+        break;
+    case 2: cout<<"no. is not found"<<endl;
+         break;
+    default:cout<< "nothing to do"<<endl;
+        break;
+    }
+}
